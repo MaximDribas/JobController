3d3a425926c5eea3db4240675cdecda1e7b6748c
Maxim Dribas <mvdribas@gmail.com>
2018-07-26 12:05:59 +0300

    Added CompanyControllerTest.

---
+++ src/test/java/com/application/controller/CompanyControllerTest.java
@@ -0,0 +1,65 @@
+package com.application.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class CompanyControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Before
+    public void setup() {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(webApplicationContext)
+                .apply(springSecurity())
+                .build();
+    }
+
+    @Test
+    public void shouldReturnIndexTemplate_WhenGetRequestWithoutAuthorized() throws Exception {
+        mockMvc.perform(get("/index"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
+    }
+
+    @Test
+    public void shouldReturnsSuccessTemplate_WhenGetRequestAuthorized() throws Exception {
+        mockMvc
+                .perform(get("/allCompanies").with(
+                        user("user").password("password").roles("USER")))
+                .andExpect(status().isOk())
+                .andExpect(view().name("allCompanies"))
+                .andExpect(content().contentType("text/html; charset=UTF-8"));
+    }
+
+    @Test
+    public void shouldRedirectToLoginError_WhenUserIsNotRegistered() throws Exception {
+        mockMvc.perform(formLogin().user("wrong_user").password("wrong_password"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/login?error"));
+    }
+}
